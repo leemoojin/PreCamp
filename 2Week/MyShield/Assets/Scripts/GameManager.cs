330192661efe895eed23173850b42ebdad9e127d
leemoojin <hzm_deli@naver.com>
2024-03-28 22:33:26 +0900

    2Week 240328

--- 2Week/MyShield/Assets/Scripts/GameManager.cs
+++ 2Week/MyShield/Assets/Scripts/GameManager.cs
@@ -10,8 +10,13 @@ public class GameManager : MonoBehaviour
 
     public Text timeTxt;
     public Text nowScore;
+    public Text bestScore;
+
+    public Animator anim;
+
 
     float time = 0.0f;
+    string key = "bestScore";
 
     bool isPlay = true;
 
@@ -59,12 +64,45 @@ public class GameManager : MonoBehaviour
     public void GameOver() 
     {
         isPlay = false;
+        anim.SetBool("isDie", true);
+
+
         //게임 정지(종료)
-        Time.timeScale = 0f;
+        Invoke("TimeStop", 0.5f);
+        
         nowScore.text = time.ToString("N2");
+
+        //최고점수가 있다면  
+        if (PlayerPrefs.HasKey(key))
+        {
+            float best = PlayerPrefs.GetFloat(key);
+            //최고점수와 현재점수를 비교, 현재 점수가 더 클때 
+            if (time > best)
+            {
+                //현재 점수를 최고점수로 등록
+                PlayerPrefs.SetFloat(key, time);
+            }
+
+        }
+        //최고점수가 없다면
+        else 
+        {
+            //현재점수를 최고점수로 등록
+            PlayerPrefs.SetFloat(key, time);
+        }
+
+        //최고점수를 엔드판넬에 적용
+        bestScore.text = PlayerPrefs.GetFloat(key).ToString("N2");
+
         //엔드판넬 표시
         endPanel.SetActive(true);
 
+    }
+
+    void TimeStop()
+    {
+        //게임 정지(종료)
+        Time.timeScale = 0f;
     }
 
 }
